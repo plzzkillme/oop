2ab01e5a6c2c73f7f8e8bcfd951ba9193ddeaf13
plzkillme <124336566+plzzkillme@users.noreply.github.com>
2025-04-15 11:38:08 +0800

    Create vehicle.py
    create vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,10 @@
+class Vehicle:
+  def start_engine(self):
+    print("vehicle start engine")
+class Car(Vehicle):
+  def car_start_engine(self):
+    print("car start engine")
+class Motorcycle(Vehicle):
+  def motorcycle_start_engine(self):
+    print("motorcycle start engine")
+    
